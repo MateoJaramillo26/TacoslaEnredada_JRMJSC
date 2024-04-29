7e39de328dc6c7900ff8fc37ee4b064f60e01db4
JeffKun1999 <jefferson.ruiz.pico@udla.edu.ec>
2024-04-28 22:11:46 -0500

    BD

--- Migrations/20240429023954_BDTabla.Designer.cs
+++ Migrations/20240429023954_BDTabla.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace TacoslaEnredada_JRMJSC.Migrations
 {
     [DbContext(typeof(UsuarioDb))]
-    partial class UsuarioDbModelSnapshot : ModelSnapshot
+    [Migration("20240429023954_BDTabla")]
+    partial class BDTabla
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -42,9 +45,6 @@ namespace TacoslaEnredada_JRMJSC.Migrations
 
                     b.Property<decimal>("Precio")
                         .HasColumnType("decimal(18,2)");
-
-                    b.Property<string>("RutaImagen")
-                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
---
+++ Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TacoslaEnredada_JRMJSC.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Nombre { get; set; }
+        [Required]
+        public string? Descripcion { get; set; }
+        [Range(0.01, 10.99)]
+        public decimal Precio { get; set; }
+        [Required]
+        public string? Ingredientes { get; set; }
+        [Display(Name = "Ruta de la imagen")]
+        public string? RutaImagen { get; set; }
+    }
+}
